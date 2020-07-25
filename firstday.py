2fd66f65e9f8cb9017428b67ba45c0001b4c5788
用户名 <>
2020-07-25 08:50:57 +0800

    在线修改

--- firstday.py
+++ firstday.py
@@ -1,2 +1,6 @@
-print('hello world!!!')
-print('please tell me ,what \' you want?')
+import numpy as np
+import pandas as pd
+a = np.arange(12).reshape(3, 4)
+print(a)
+b = pd.Series([1, 12, 23, 4, 5])
+print(b)
